99c45f36a73af29b1894695db8e24b03b569940b
Prince Narteh <prinarteh@gmail.com>
2024-01-10 02:25:21 +0000

    feat: add header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,5 +1,29 @@
+import Link from "next/link";
 import React from "react";
 
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav>
+        <div className="navbar justify-between bg-base-300">
+          <Link href="/" className="btn btn-ghost text-lg">
+            Next Amazona V2
+          </Link>
+
+          <ul className="flex">
+            <li>
+              <Link href="/cart" className="btn btn-ghost rounded-btn">
+                Cart
+              </Link>
+            </li>
+            <li>
+              <Link href="/cart" className="btn btn-ghost rounded-btn">
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 };
