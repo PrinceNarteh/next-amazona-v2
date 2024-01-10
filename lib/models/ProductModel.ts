3d1b2ed8c1df204b3b2990fa17e8f718e93c8c54
Prince Narteh <prinarteh@gmail.com>
2024-01-10 03:25:29 +0000

    feat: display product list

--- lib/models/ProductModel.ts
+++ lib/models/ProductModel.ts
@@ -1,16 +1,17 @@
 export type Product = {
-  _id: string;
+  _id?: string;
   name: string;
   slug: string;
   image: string;
-  banner: string;
-  price: string;
+  banner?: string;
+  price: number;
   brand: string;
   description: string;
   category: string;
   rating: number;
   numReviews: number;
   countInStock: number;
+  isFeature?: boolean;
   colors?: string[];
   sizes?: string[];
 };
